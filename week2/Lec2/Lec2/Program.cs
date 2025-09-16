e898ed85e832b3da29876b22509f173162da2359
Sergiu Ghimp <ghimpsergiu@gmail.com>
2025-09-16 10:47:16 +0100

    Lecture 2 & practice implementation

---
+++ week2/Lec2/Lec2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Lec2
+{
+    class ClassName {
+        // Fields
+        public int abc;
+        private int field;
+
+        // Constructor
+        public ClassName(int paramm) { 
+            field = paramm;
+            // abc = param2;
+        }
+
+        // Method
+        public void MethodName()
+        {
+            Console.WriteLine(abc);
+            Console.WriteLine("This is a method.");
+        }
+        
+        public void AnotherMethod(){
+            MethodName();
+        }
+        
+    }    
+
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            ClassName obj1 = new ClassName(10);
+            obj1.abc = 5;
+            obj1.MethodName();
+            // Console.WriteLine(obj1.abc);
+        }
+    }
+}
